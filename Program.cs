e0911c014ce9debfafd6f69da6192bb8e422b0f2
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-30 14:58:23 +0530

    [Prashant] add Program.cs file

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Annotations.CustomAttributes;
+using Annotations.LoggingMethod;
+using Annotations.MarkImportant;
+using Annotations.MaxLengthAttributeDemo;
+using Annotations.RepeatableAttribute;
+using Annotations.RoleBasedAccess;
+using Annotations.Todo;
+using Annotations.UseMethodOverriding;
+namespace Annotations
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Animal dog = new Dog();
+            // dog.MakeSound();
+
+            // LegacyAPI.OldFeature(); // gives warning
+            // LegacyAPI.NewFeature();
+
+            // RetrieveAttribute.Execute();
+
+            // DemoRepeatable.Execute();
+
+            // MarkMain.Execute();
+
+            // TodoMain.Execute();
+
+            // WorkMain.Execute();
+
+            // MaxMain.Execute();
+
+            // RoleAccessMain.Execute();
+
+            
+        }
+    }
+}
